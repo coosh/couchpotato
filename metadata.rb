24711c4fdb5ab1f875df0e39b9d7b2acf43f878d
Robert Postill <robert@shopify.com>
2014-03-02 01:03:40 -0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Eric Helgeson"
 maintainer_email "erichelgeson@gmail.com"
 license          "Apache 2.0"
 description      "Installs and configures CouchPotato onto a node"
-version          "0.0.1"
+version          "0.0.2"
  
 %w{ ubuntu }.each do |os|
   supports os
